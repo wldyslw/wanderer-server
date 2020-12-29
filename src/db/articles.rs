632c0c72f083a4da5e2a31f4c4d6c8105d42352e
Uladzislau <wldyslw@outlook.com>
2020-12-29 14:19:18 +0300

    Models moved to separate module

--- src/db/articles.rs
+++ src/db/articles.rs
@@ -1,62 +1,8 @@
-use chrono::prelude::*;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
-use serde::Serialize;
 
-use crate::config::DATETIME_FORMAT_ARTICLE;
+use crate::models::article::*;
 use crate::schema::articles;
-
-#[derive(Queryable)]
-pub struct Article {
-    pub id: i32,
-    pub slug: String,
-    pub title: String,
-    pub title_image: String,
-    pub description: String,
-    pub body: String,
-    pub tag_list: Vec<String>,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>,
-    pub favorites_count: i32,
-}
-
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ArticleJson {
-    pub id: i32,
-    pub slug: String,
-    pub title: String,
-    pub title_image: String,
-    pub description: String,
-    pub body: String,
-    pub tag_list: Vec<String>,
-    pub created_at: String,
-    pub updated_at: String,
-    pub favorites_count: i32,
-}
-
-impl Into<ArticleJson> for Article {
-    fn into(self) -> ArticleJson {
-        ArticleJson {
-            id: self.id,
-            slug: self.slug,
-            title: self.title,
-            title_image: self.title_image,
-            description: self.description,
-            body: self.body,
-            tag_list: self.tag_list,
-            created_at: self
-                .created_at
-                .format_localized(DATETIME_FORMAT_ARTICLE, Locale::be_BY)
-                .to_string(),
-            updated_at: self
-                .updated_at
-                .format_localized(DATETIME_FORMAT_ARTICLE, Locale::be_BY)
-                .to_string(),
-            favorites_count: self.favorites_count,
-        }
-    }
-}
 
 pub fn all(connection: &PgConnection) -> Vec<ArticleJson> {
     let articles = articles::table
